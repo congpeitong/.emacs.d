c50e526bc6f5d25143fc03cd50479adb84535f13
congpeitong <congpeitong2022@163.com>
2022-07-11 09:23:42 +0800

    自动保存间隔10秒

--- config/init-auto-save.el
+++ config/init-auto-save.el
@@ -1,6 +1,6 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/config/package"))
 (require 'auto-save) ;; 加载自动保存配置文件
-(setq auto-save-idle 5) ;; 空闲间隔保存(默认1秒),必须定义在开启自动保存前
+(setq auto-save-idle 10) ;; 空闲间隔保存(默认1秒),必须定义在开启自动保存前
 (auto-save-enable) ;; 开启自动保存
 (setq auto-save-silent t) ;; 悄悄地保存弹框的不要
 (setq auto-save-delete-trailing-whitespace t) ;;保存时自动删除行尾的空格，不想删除则可以赋值 nil
